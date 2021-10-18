d99c9a63fc7598c4e7890fce9cbf5b70b538a661
Abadbeginning <2608403303@qq.com>
2021-10-18 01:52:50 +0000

    4-7 不定项居中布局-补充

--- src/flex/4_7/1demo.tsx
+++ src/flex/4_7/1demo.tsx
@@ -3,9 +3,11 @@ import "../../asserts/css/flex/4_7/1demo.css";
 export const Flex4 = () => {
     return <div>
         <div className={"box"}>
-            <div></div>
-            <div></div>
-            <div></div>
+            <section>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </section>
         </div>
     </div>
 }
