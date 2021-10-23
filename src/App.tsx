b8c69ae024d7c95f8e4dc3a9cf4fa1ecfd0d1190
Abadbeginning <2608403303@qq.com>
2021-10-23 06:09:50 +0000

    4-15 Sticky Footer布局

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ import {Flex} from './flex/4_12/2demo'
 import {FlexOrder} from './flex/4_12/3demo'
 import {FlexContour} from './flex/4_13/1demo'
 import {FlexColom} from './flex/4_14/1demo'
+import {FlexStickyFooter} from './flex/4_15/1demo'
 
 function App() {
 
@@ -63,7 +64,8 @@ function App() {
       {/* <Flex /> */}
       {/* <FlexOrder /> */}
       {/* <FlexContour /> */}
-      <FlexColom />
+      {/* <FlexColom /> */}
+      <FlexStickyFooter />
       
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
