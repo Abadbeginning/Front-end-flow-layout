8f63c905cc01896ac9c772d1d1670197c0a2fc8d
e <2029618609@qq.com>
2021-11-16 16:14:20 +0800

    5-14 综合案例二(小米商品导航菜单)

--- src/grid/5_14/1.tsx
+++ src/grid/5_14/1.tsx
@@ -1,7 +1,242 @@
 import "../../asserts/css/grid/5_14/1.css"
+import "../../asserts/css/grid/5_14/iconfont.css"
+import Phone from "../../asserts/css/grid/5_14/phone.png"
 
 export const Grid16 = () => {
     return <div>
-        <div>1</div>
+        <ul className={"nav"}>
+            <li>
+                <div>
+                    <a href="#">手机</a>
+                    <a href="#">电话卡</a>
+                </div>
+                <i className={"iconfont icon-right-arrow"}></i>
+
+                
+            <ul className={"nav-menu"}>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+            </ul>
+
+            </li>
+
+            <ul className={"nav-menu"}>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+                <li>
+                    <img src={Phone} alt="" />
+                    <p>小米 MIX FOLD</p>
+                </li>
+            </ul>
+
+            <li>
+                <div>
+                    <a href="#">手机</a>
+                    <a href="#">电话卡</a>
+                </div>
+                <i className="iconfont icon-right-arrow"></i>
+                <ul className={"nav-menu"}>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <div>
+                    <a href="#">笔记本</a>
+                    <a href="#">显示器</a>
+                    <a href="#">平板</a>
+                </div>
+                <i className="iconfont icon-right-arrow"></i>
+                <ul className={"nav-menu"}>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <div>
+                    <a href="#">手机</a>
+                    <a href="#">电话卡</a>
+                </div>
+                <i className="iconfont icon-right-arrow"></i>
+                <ul className={"nav-menu"}>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                    <li>
+                        <img src={Phone} alt="" />
+                        <p>小米 MIX FOLD</p>
+                    </li>
+                </ul>
+            </li>
+            
+            <li>
+                <div>
+                    <a href="#">笔记本</a>
+                    <a href="#">显示器</a>
+                    <a href="#">平板</a>
+                </div>
+                <i className="iconfont icon-right-arrow"></i>
+            </li>
+
+            <li>
+                <div>
+                    <a href="#">手机</a>
+                    <a href="#">电话卡</a>
+                </div>
+                <i className="iconfont icon-right-arrow"></i>
+            </li>
+
+        </ul>
     </div>
 }
