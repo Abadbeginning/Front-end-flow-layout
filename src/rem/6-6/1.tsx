6a33a54e4839a5a03db550a9c0906660cc02805c
e <2029618609@qq.com>
2021-11-20 16:44:45 +0800

    6-7 rem案例：网易移动端导航实现

--- src/rem/6-6/1.tsx
+++ src/rem/6-6/1.tsx
@@ -19,5 +19,19 @@ export const Rem3 = () => {
                 <i className={"iconfont icon-jiantou"}></i>
             </div>
         </div>
+        <ul className={"nav"}>
+            <li className={"active"}>
+                <a href="#">要闻</a>
+            </li>
+            <li>
+                <a href="#">推荐</a>
+            </li>
+            <li>
+                <a href="#">原创</a>
+            </li>
+            <li>
+                <a href="#">热点</a>
+            </li>
+        </ul>
     </div>
 }
