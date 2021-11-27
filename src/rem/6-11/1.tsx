0a54fa6c17773e8f8588658faf79045928083ea5
Abadbeginning <2608403303@qq.com>
2021-11-27 08:52:09 +0000

    6-13 vw案例：B站移动端视频列表实现 上

--- src/rem/6-11/1.tsx
+++ src/rem/6-11/1.tsx
@@ -1,10 +1,14 @@
 import "../../asserts/css/rem/6-11/1.css"
 import "../../asserts/css/rem/6-11/iconfont/iconfont.css"
 import Video1 from "../../asserts/css/rem/6-11/img/video1.jpg"
+import Video2 from "../../asserts/css/rem/6-11/img/video2.jpg"
+import Video3 from "../../asserts/css/rem/6-11/img/video3.jpg"
+import Video4 from "../../asserts/css/rem/6-11/img/video4.jpg"
 
 export const Rem5 = () => {
     return <div>
         <div className={"bilibili"}>
+            {/* 头部 */}
             <div className={"head"}>
                 <div className={"head-logo"}>
                     <i className={"iconfont icon-BILIBILI_LOGO"}></i>
@@ -23,6 +27,7 @@ export const Rem5 = () => {
                 </div>
             </div>
 
+            {/* 导航栏 */}
             <div className={"nav"}>
                 <ul className={"nav-list"}>
                     <li className={"nav-list-active"}>
@@ -55,6 +60,238 @@ export const Rem5 = () => {
                     <i className="iconfont icon-arrow-bottom"></i>
                 </div>
             </div>
+
+            {/* 视频详情 */}
+            <ul className={"video"}>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video1} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">那些语文课本里的已删节</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video2} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">吃我一记大桶水</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video3} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">那些语文课本里的已删节</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video4} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">吃我一记大桶水</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video1} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">那些语文课本里的已删节</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video2} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">吃我一记大桶水</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video3} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">那些语文课本里的已删节</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video4} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">吃我一记大桶水</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video1} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">那些语文课本里的已删节</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video2} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">吃我一记大桶水</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video3} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">那些语文课本里的已删节</a>
+                    </h3>
+                </li>
+                <li className={"video-item"}>
+                    <div className={"video-item-pic"}>
+                        <img src={Video4} alt="" />
+                        
+                        <span>
+                            <div>
+                                <i className="iconfont icon-703bofang-fangxing-xianxing"></i>
+                                73.7万
+                            </div>
+                            <div>
+                                <i className="iconfont icon-xinxi"></i>
+                                5591
+                            </div>    
+                        </span>
+                    </div>
+                    <h3 className="video-item-title">
+                        <a href="#">吃我一记大桶水</a>
+                    </h3>
+                </li>
+            </ul>
         </div>
     </div>
 }
