06ebdc66c78e0ddb7ba1e12574b6599a9ad4edea
Abadbeginning <2608403303@qq.com>
2021-11-27 07:29:05 +0000

    6-12 vw案例：B站移动端导航实现 以及注释添加

--- src/rem/6-11/1.tsx
+++ src/rem/6-11/1.tsx
@@ -22,6 +22,39 @@ export const Rem5 = () => {
                     <a href="#">下载 APP</a>
                 </div>
             </div>
+
+            <div className={"nav"}>
+                <ul className={"nav-list"}>
+                    <li className={"nav-list-active"}>
+                        <a href="#">首页</a>
+                    </li>
+
+                    <li>
+                        <a href="#">动画</a>
+                    </li>
+                    <li>
+                        <a href="#">番剧</a>
+                    </li>
+                    <li>
+                        <a href="#">国创</a>
+                    </li>
+                    <li>
+                        <a href="#">音乐</a>
+                    </li>
+                    <li>
+                        <a href="#">舞蹈</a>
+                    </li>
+                    <li>
+                        <a href="#">漫画</a>
+                    </li>
+                    <li>
+                        <a href="#">赛事</a>
+                    </li>
+                </ul>
+                <div className="nav-arrow">
+                    <i className="iconfont icon-arrow-bottom"></i>
+                </div>
+            </div>
         </div>
     </div>
 }
