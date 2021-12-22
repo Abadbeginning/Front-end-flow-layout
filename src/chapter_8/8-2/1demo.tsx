4b34a1e21f7db2d0e5f7d1ee8932e67b02476f05
Abadbeginning <2608403303@qq.com>
2021-12-22 08:55:47 +0000

    改变logo大小 图标标题居中

--- src/chapter_8/8-2/1demo.tsx
+++ src/chapter_8/8-2/1demo.tsx
@@ -1,5 +1,6 @@
 import "asserts/css/chapter_8/8-2/global.css"
 import "asserts/css/chapter_8/8-2/index.css"
+import "asserts/css/chapter_8/iconfont/iconfont.css"
 import Logo from "asserts/css/chapter_8/static/logo.svg"
 
 export const AntPro = () => {
@@ -7,13 +8,112 @@ export const AntPro = () => {
     <div className={"g-ant"}>
       <div className="g-ant__sider">
       {/* <div className="g-ant__sider g-ant__sider--closed"> */}
-        <div className="g-ant__sider__wrap g-ant__sider__wrap--fixed">
-           <div className="g-ant__sider__head">
-             <a href="#">
+        <div className="g-ant-sider__wrap g-ant-sider__wrap--fixed">
+           <div className="g-ant-sider__head">
+             <a className="m-logo" href="#">
               <img src={Logo} alt="" />
               <h2>Ant Design Pro</h2> 
              </a>
              
+           </div>
+
+           <ul className="g-ant-sider__main">
+             {/* 是否被选中 */}
+              <li className="m-menu m-menu--selected">
+                <div className="m-menu__title">
+                  <i className="iconfont icon-car"></i>
+                  <span>Dashboard</span>
+                  <i className="iconfont icon-arrowup"></i>
+                </div>
+                <ul className="m-menu__sub">
+                  <li>分析页</li>
+                  <li className="m-menu__sub--selected">监控页</li>
+                  <li>工作台</li>
+                </ul>
+              </li>
+              
+              <li className="m-menu">
+                <div className="m-menu__title">
+                  <i className="iconfont icon-car"></i>
+                  <span>Dashboard</span>
+                  <i className="iconfont icon-arrowup"></i>
+                </div>
+                <ul className="m-menu__sub">
+                  <li>分析页</li>
+                  <li className="m-menu__sub--selected">监控页</li>
+                  <li>工作台</li>
+                </ul>
+              </li>
+              
+              <li className="m-menu">
+                <div className="m-menu__title">
+                  <i className="iconfont icon-car"></i>
+                  <span>Dashboard</span>
+                  <i className="iconfont icon-arrowup"></i>
+                </div>
+                <ul className="m-menu__sub">
+                  <li>分析页</li>
+                  <li className="m-menu__sub--selected">监控页</li>
+                  <li>工作台</li>
+                </ul>
+              </li>
+              
+              <li className="m-menu">
+                <div className="m-menu__title">
+                  <i className="iconfont icon-car"></i>
+                  <span>Dashboard</span>
+                  <i className="iconfont icon-arrowup"></i>
+                </div>
+                <ul className="m-menu__sub">
+                  <li>分析页</li>
+                  <li className="m-menu__sub--selected">监控页</li>
+                  <li>工作台</li>
+                </ul>
+              </li>
+              
+              <li className="m-menu">
+                <div className="m-menu__title">
+                  <i className="iconfont icon-car"></i>
+                  <span>Dashboard</span>
+                  <i className="iconfont icon-arrowup"></i>
+                </div>
+                <ul className="m-menu__sub">
+                  <li>分析页</li>
+                  <li className="m-menu__sub--selected">监控页</li>
+                  <li>工作台</li>
+                </ul>
+              </li>
+              
+              <li className="m-menu">
+                <div className="m-menu__title">
+                  <i className="iconfont icon-car"></i>
+                  <span>Dashboard</span>
+                  <i className="iconfont icon-arrowup"></i>
+                </div>
+                <ul className="m-menu__sub">
+                  <li>分析页</li>
+                  <li className="m-menu__sub--selected">监控页</li>
+                  <li>工作台</li>
+                </ul>
+              </li>
+              
+              <li className="m-menu">
+                <div className="m-menu__title">
+                  <i className="iconfont icon-car"></i>
+                  <span>Dashboard</span>
+                  <i className="iconfont icon-arrowup"></i>
+                </div>
+                <ul className="m-menu__sub">
+                  <li>分析页</li>
+                  <li className="m-menu__sub--selected">监控页</li>
+                  <li>工作台</li>
+                </ul>
+              </li>
+              
+           </ul>
+
+           <div className="g-ant-sider__foot">
+            <i className="iconfont icon-outdent u-bar"></i>
            </div>
         </div>
       </div>
