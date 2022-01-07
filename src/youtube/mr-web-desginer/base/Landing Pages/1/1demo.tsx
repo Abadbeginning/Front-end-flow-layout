60abcda5e02b0a39c365b28ff89ba4b55cca1e4e
Abadbeginning <2029618609@qq.com>
2022-01-07 21:03:42 +0800

    youtube page1 demo 上

--- src/youtube/mr-web-desginer/base/Landing Pages/1/1demo.tsx
+++ src/youtube/mr-web-desginer/base/Landing Pages/1/1demo.tsx
@@ -4,7 +4,36 @@ import Bro from "asserts/css/youtube/mr-web-desginer/base/Landing Pages/1/img/Cr
 export const Page = () => {
   return <div>
     <div className="container">
-      <img src={Bro} alt="" />
+      <div className="content">
+        <h3 className="title">we are coming soon!</h3>
+        <p>how to make a complete responsive coming soon landing page website design using html css and vanilla javascript with count down clock effect.
+create a complete responsive coming soon page UI design with count down effect</p>
+
+        <div className="count-down">
+          <div className="box">
+            <h3 id="day">00</h3>
+            <span>day</span>
+          </div>
+          <div className="box">
+            <h3 id="hour">00</h3>
+            <span>hour</span>
+          </div>
+          <div className="box">
+            <h3 id="minute">00</h3>
+            <span>minute</span>
+          </div>
+          <div className="box">
+            <h3 id="second">00</h3>
+            <span>second</span>
+          </div>
+        </div>
+
+        <a href="#" className="btn">notify me</a>
+
+      </div>
+      <div className="image">
+        <img src={Bro} alt="" />
+      </div>
     </div>
   </div>
 }
